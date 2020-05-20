6a81b9945d50ba0976300fedbb40bafffa8e33a2
RuslanMalyugin <60871879+RuslanMalyugin@users.noreply.github.com>
2020-05-20 04:43:58 +0300

    Update serv.py

--- serv.py
+++ serv.py
@@ -1,11 +1,11 @@
 import json
-import string
-import random
 
-import flask
 from flask import Flask, request
 
-app = Flask(__name__)
+app = Flask("Shesterochka")
+
+adress = "localhost"
+port = 5000
 
 
 def parse_food(food, process):
@@ -17,6 +17,26 @@ def parse_food(food, process):
     return order
 
 
+def single_form(upper_text, button, tag):
+    return f"""<form method="POST">
+            {upper_text}<br>
+            <input type="text" name={tag}><br>
+            <input type="submit" value={button}>
+            </form>"""
+
+
+def double_form(first_text, button, first_tag, second_text, second_tag):
+    return f"""<form method="post">
+        <p>
+	    <label for="first_label">{first_text}</label> <br>
+	    <input type="first" name={first_tag}> 
+	 </p>
+	    <label for="second_label">{second_text}</label> <br>
+	    <input type="second" name={second_tag}> <br>
+	     <input type="submit" value={button}>
+    </form>"""
+
+
 def parse_new_food(food):
     splitted = food.split(', ')
     order = {}
@@ -26,26 +46,21 @@ def parse_new_food(food):
 
 
 def login(test):
-    password = ''
     with open("password.txt", "r") as file_name:
-        for line in file_name:
-            password = line
-    if str(test) == password:
-        return True
-    else:
-        return False
+        password = file_name.read()
+    return str(test) == password
 
 
 @app.route('/')
 def start_page():
-    text1 = """<p> Shop! </p>
+    text1 = f"""<p> Shop! </p>
     <p> Commands: </p>
-    <p> 1. <a   href="http://127.0.0.1:5000/offers" > /offers </a> - show food and offers   </p>
-    <p> 2. <a  href="http://127.0.0.1:5000/check" > /check </a> - check your order </p>
-    <p> 3. <a  href="http://127.0.0.1:5000/add" > /add </a> - add food to shop (admin only) </p>
-    <p> 4. <a  href="http://127.0.0.1:5000/show_orders" >/show_orders </a> - show all orders (admin only) </p>
-    <p> 5. <a  href="http://127.0.0.1:5000/change_status" >/change_status </a> - change status order's id (admin only) </p>
-    <p> 6. <a  href="http://127.0.0.1:5000/change_password" >/change_password </a> - change password (admin only) </p>
+    <p> 1. <a   href="http://{adress}:{port}/offers" > /offers </a> - show food and offers   </p>
+    <p> 2. <a  href="http://{adress}:{port}/check" > /check </a> - check your order </p>
+    <p> 3. <a  href="http://{adress}:{port}/add" > /add </a> - add food to shop (admin only) </p>
+    <p> 4. <a  href="http://{adress}:{port}/show_orders" >/show_orders </a> - show all orders (admin only) </p>
+    <p> 5. <a  href="http://{adress}:{port}/change_status" >/change_status </a> - change status order's id (admin only) </p>
+    <p> 6. <a  href="http://{adress}:{port}/change_password" >/change_password </a> - change password (admin only) </p>
     """
     return str(text1)
 
@@ -57,11 +72,8 @@ def get_offer():
         text = ""
         for i in data:
             text = " ".join([text, "<p>", str(i), str(data[i]), "$", "</p>"])
-        text = " ".join([text, """<form method="POST">
-        Your order in format (Name, food, amount, food2, amount...) :<br>
-        <input type="text" name="tag"><br>
-        <input type="submit" value="order">
-        </form>"""])
+        text = " ".join(
+            [text, single_form("Your order in format (Name, food, amount, food2, amount...) :", "order", "tag")])
         if request.method == 'POST':
             tag = request.form['tag']
             current_order = parse_food(tag, "In process")
@@ -70,17 +82,13 @@ def get_offer():
                 orders.update(current_order)
             with open("offers.json", "w") as writing_file:
                 json.dump(orders, writing_file, indent=2)
-    text = " ".join([text, '<p> <a   href= "http://127.0.0.1:5000/" > Go back </a> </p> '])
+    text = " ".join([text, f'<p> <a   href= "http://{adress}:{port}" > Go back </a> </p> '])
     return text
 
 
 @app.route('/check/', methods=['GET', 'POST'])
 def checking():
-    text = """<form method="POST">
-            Your name :<br>
-            <input type="text" name="tag"><br>
-            <input type="submit" value="check">
-            </form>"""
+    text = single_form("Your name :", "check", "tag")
     if request.method == 'POST':
         tag = request.form['tag']
         with open("offers.json", "r") as reading_file:
@@ -90,21 +98,13 @@ def checking():
         else:
             status = "No orders!"
         text = " ".join([text, status])
-    text = " ".join([text, '<p> <a   href= "http://127.0.0.1:5000/" > Go back </a> </p> '])
+    text = " ".join([text, f'<p> <a   href= "http://{adress}:{port}" > Go back </a> </p> '])
     return text
 
 
 @app.route('/add/', methods=['GET', 'POST'])
 def add():
-    text = """<form method="post">
-        <p>
-	    <label for="password">Password</label> <br>
-	    <input type="pswrd" name="password"> 
-	 </p>
-	    <label for="food">New food in format(Food1, cost1, ..) </label> <br>
-	    <input type="food" name="new_food"> <br>
-	     <input type="submit" value="add">
-    </form>"""
+    text = double_form("Password", "add", "password", "New food in format(Food1, cost1, ..) ", "new_food")
     if request.method == 'POST':
         password = request.form.get("password")
         if not login(password):
@@ -118,19 +118,15 @@ def add():
             with open("data_file.json", "w") as writing_file:
                 json.dump(orders, writing_file, indent=2)
             text = " ".join([text, '<p> Added! </p>'])
-    text = " ".join([text, '<p> <a   href= "http://127.0.0.1:5000/" > Go back </a> </p> '])
+    text = " ".join([text, f'<p> <a   href= "http://{adress}:{port}" > Go back </a> </p> '])
     return text
 
 
 @app.route('/show_orders/', methods=['GET', 'POST'])
 def show_orders():
-    text = """<form method="post">
-    	    <label for="password">Password</label> <br>
-	    <input type="pswrd" name="password">  <br>
-    	     <input type="submit" value="check">
-        </form>"""
+    text = single_form("Password", "check", "tag")
     if request.method == 'POST':
-        password = request.form.get("password")
+        password = request.form["tag"]
         if not login(password):
             text = " ".join([text, '<p> Access denied! </p>'])
         else:
@@ -144,21 +140,13 @@ def show_orders():
                         else:
                             text = " ".join([text, ",", str(j), str(data[i][j]), "pieces"])
                     text = " ".join([text, "</p>"])
-    text = " ".join([text, '<p> <a   href= "http://127.0.0.1:5000/" > Go back </a> </p> '])
+    text = " ".join([text, f'<p> <a   href= "http://{adress}:{port}" > Go back </a> </p> '])
     return text
 
 
 @app.route('/change_status/', methods=['GET', 'POST'])
 def change_status():
-    text = """<form method="post">
-            <p>
-    	    <label for="password">Password</label> <br>
-    	    <input type="pswrd" name="password"> 
-    	 </p>
-    	    <label for="change">Changes in format(Name1, change1, ..) </label> <br>
-    	    <input type="change" name="changes"> <br>
-    	     <input type="submit" value="change">
-        </form>"""
+    text = double_form("Password", "change", "password", "Changes in format(Name1, change1, ..) ", "changes")
     if request.method == 'POST':
         password = request.form.get("password")
         if not login(password):
@@ -177,20 +165,13 @@ def change_status():
                 text = " ".join([text, '<p>  Changed! </p>'])
             else:
                 text = " ".join([text, '<p>  Not found! </p>'])
-    text = " ".join([text, '<p> <a   href= "http://127.0.0.1:5000/" > Go back </a> </p> '])
+    text = " ".join([text, f'<p> <a   href= "http://{adress}:{port}" > Go back </a> </p> '])
     return text
+
 
 @app.route('/change_password/', methods=['GET', 'POST'])
 def change_password():
-    text = """<form method="post">
-                <p>
-        	    <label for="password">Old password</label> <br>
-        	    <input type="pswrd" name="password"> 
-        	 </p>
-        	    <label for="change">New password </label> <br>
-        	    <input type="change" name="password2"> <br>
-        	     <input type="submit" value="change">
-            </form>"""
+    text = double_form("Old password", "change", "password", "New password", "password2")
     if request.method == 'POST':
         password = request.form.get("password")
         if not login(password):
@@ -200,11 +181,9 @@ def change_password():
             with open("password.txt", "w") as writing_lines:
                 writing_lines.write(password2)
             text = " ".join([text, '<p> Changed! </p>'])
-    text = " ".join([text, '<p> <a   href= "http://127.0.0.1:5000/" > Go back </a> </p> '])
+    text = " ".join([text, f'<p> <a   href= "http://{adress}:{port}" > Go back </a> </p> '])
     return text
 
 
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(adress, port=port, debug=True)
